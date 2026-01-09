59a1bd2f7f272743a5b2a17475a721b0b77c25f8
ayush <axlee484@users.noreply.github.com>
2026-01-09 14:14:17 +0530

    add eagle enemy

---
+++ enemy/eagle/eagle.gd
@@ -0,0 +1,57 @@
+extends EnemyBase
+
+
+@onready var animatedSprite = $AnimatedSprite2D
+@onready var directionTimer = $DirectionTimer
+
+
+@export var EAGLE_FLY_SPEED: Vector2 = Vector2(75, 50)
+
+
+var isVisibleOnScreen = false
+
+func seePlayer():
+    if !isVisibleOnScreen:
+        return
+    var playerPos = playerRef.global_position.x
+    var selfPos = global_position.x
+
+    if playerPos <= selfPos:
+        FACING = FACE_DIRECTION.LEFT
+    else:
+        FACING = FACE_DIRECTION.RIGHT
+    animatedSprite.flip_h = FACING == FACE_DIRECTION.RIGHT
+
+
+
+
+func _ready() -> void:
+    super._ready()
+    seePlayer()
+
+
+func flyToPlayer():
+    velocity = EAGLE_FLY_SPEED
+    velocity.x *= FACING
+
+
+
+func _physics_process(_delta: float) -> void:
+    super._physics_process(_delta)
+    print(global_position)
+    move_and_slide()
+
+
+
+
+func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
+    seePlayer()
+    animatedSprite.play("fly")
+    directionTimer.start()
+    isVisibleOnScreen = true
+
+
+func _on_direction_timer_timeout() -> void:
+    seePlayer()
+    flyToPlayer()
+    directionTimer.start()
